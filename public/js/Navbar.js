ec27825f29fde67eb63368f28f0ed55b64ca2084
Willerjls <willer.jacques555@gmail.com>
2025-02-19 23:36:50 -0300

    Menu

--- public/js/Navbar.js
+++ public/js/Navbar.js
@@ -1,9 +1,16 @@
-const escondeDropdown = () => {
+const escondeDropdown = (event) => {
   const dropdown_navbar = document.querySelector(".dropdown_navbar");
-  if (dropdown_navbar.style.display === "none") {
-    dropdown_navbar.style.display = "flex";
-  }
-  else {
-    dropdown_navbar.style.display = "none";
-  }
+  dropdown_navbar.style.display = dropdown_navbar.style.display === "flex" ? "none" : "flex";
+  event.stopPropagation();
 }
+
+document.addEventListener("click", (event) => {
+  const dropdown_navbar = document.querySelector(".dropdown_navbar");
+  if (dropdown_navbar.style.display === "flex" && event.target !== dropdown_navbar) {
+    dropdown_navbar.style.display = "none";
+   }
+});
+
+document.querySelector(".dropdown_navbar").addEventListener("click", (event) => {
+  event.stopPropagation();
+});
